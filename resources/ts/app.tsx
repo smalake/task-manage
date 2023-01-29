49bfc45979e6944724c14bdad1518ba7aa4de519
smalake <koizumim1002@gmail.com>
2023-01-29 14:34:38 +0900

    React側のルーティング作成

--- resources/ts/app.tsx
+++ resources/ts/app.tsx
@@ -1,10 +1,26 @@
 import "../css/app.css";
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { blue } from "@mui/material/colors";
 
 const App = () => {
-    const title: string = "This is React × Laravel App";
-    return <h1>{title}</h1>;
+    const theme = createTheme({
+        palette: { primary: blue },
+    });
+    return (
+        <ThemeProvider theme={theme}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="login" element={<Login />} />
+                    <Route path="register" element={<Register />} />
+                </Routes>
+            </BrowserRouter>
+        </ThemeProvider>
+    );
 };
 
 ReactDOM.render(<App />, document.getElementById("app"));
